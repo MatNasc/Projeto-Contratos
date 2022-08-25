ed48f98796776d3659dc8003f8d2c41d04860277
joaozinn <0000887991@senaimgaluno.com.br>
2022-08-24 22:03:43 -0300

    Tela Busca LL

---
+++ Projeto-Contratos/Projeto-Contratos/TelaBusca/TelaBuscaLL.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="TelaBuscaLL.aspx.cs" Inherits="Projeto_Contratos.TelaBusca.TelaBuscaLL" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <br />
+        <div class="row">
+            <asp:TextBox runat="server" ID="txtBusca" placeholder="Digite um Nome"></asp:TextBox>
+            <asp:Button runat="server" ID="btnBusca" OnClick="btnBusca_Click" Text="Procurar" />
+        </div>
+        <br />
+        <div class="row">
+            <asp:GridView runat="server" ID="grdClientes" AutoGenerateColumns="false" OnRowCommand="grdClientes_RowCommand" Width="100%" AllowPaging="true" PageSize="20">
+                <Columns>
+                    <asp:BoundField DataField="nome" HeaderText="NOME" />
+                </Columns>
+            </asp:GridView>
+        </div>
+
+
+
+    </div>
+
+</asp:Content>
---
+++ Projeto-Contratos/Projeto-Contratos/TelaBusca/TelaBuscaLL.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Projeto_Contratos.TelaBusca {
+    
+    
+    public partial class TelaBuscaLL {
+        
+        /// <summary>
+        /// Controle txtBusca.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBusca;
+        
+        /// <summary>
+        /// Controle btnBusca.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBusca;
+        
+        /// <summary>
+        /// Controle grdClientes.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grdClientes;
+    }
+}
