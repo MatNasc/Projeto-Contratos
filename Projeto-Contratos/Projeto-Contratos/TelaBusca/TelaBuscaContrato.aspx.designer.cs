58a23d1d674e17206961778104c303af4c2709a3
Israel Venancio <0000673236@senaimgaluno.com.br>
2022-08-25 22:03:08 -0300

    Finalização da Tela de Busca de Contrato

---
+++ Projeto-Contratos/Projeto-Contratos/TelaBusca/TelaBuscaContrato.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Projeto_Contratos.TelaBusca {
+    
+    
+    public partial class TelaBuscaContrato {
+        
+        /// <summary>
+        /// Controle Consultar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Consultar;
+        
+        /// <summary>
+        /// Controle BtnPesquisar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnPesquisar;
+        
+        /// <summary>
+        /// Controle grd_Contratos.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grd_Contratos;
+    }
+}
