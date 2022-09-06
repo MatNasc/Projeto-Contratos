3bfdd64e85cb34c6425c31c0a693d78afe6e53ed
joaozinn <0000887991@senaimgaluno.com.br>
2022-09-05 22:01:47 -0300

    Back End Tela Busca de Imovel

--- Projeto-Contratos/Projeto-Contratos/TelaBusca/TelaBuscaImovel.aspx.cs
+++ Projeto-Contratos/Projeto-Contratos/TelaBusca/TelaBuscaImovel.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,26 @@ namespace Projeto_Contratos.TelaBusca
 {
     public partial class TelaBuscaImovel : System.Web.UI.Page
     {
+        private MySqlConnection connection;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            connection = new MySqlConnection(SiteMaster.ConnectionString);
+            if (!IsPostBack)
+            {
+                DataTable imovel = new DataTable();
 
+
+                connection.Open();
+
+                var comando = new MySqlCommand("SELECT id_locador,rua,numero,bairro,cidade,n_agua,n_luz,valor FROM imovel", connection);
+                var reader = comando.ExecuteReader();
+                
+                while(reader.Read())
+                {
+                    var linha = imovel.NewRow();
+                }
+            }
         }
 
         protected void btnbuscaIM_Click(object sender, EventArgs e)
