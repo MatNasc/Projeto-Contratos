44cdd486459de9d942ffc961909bb5a81baa2347
jhonatan <williamjhonatanxd@gmail.com>
2022-09-19 22:16:42 -0300

    ultima att da aula 19/09

--- Projeto-Contratos/Projeto-Contratos/Cadastros_info/Contrato.aspx.cs
+++ Projeto-Contratos/Projeto-Contratos/Cadastros_info/Contrato.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using MySqlConnector;
 namespace Projeto_Contratos.Cadastros_info
 {
+
     public partial class Contrato : System.Web.UI.Page
     {
+        private MySqlConnection connection;
+        
         protected string datainicio;
         protected string tempttllocação;
         protected string datatermino;
@@ -23,7 +26,14 @@ namespace Projeto_Contratos.Cadastros_info
 
         protected void Page_Load(object sender, EventArgs e)
         {
-          
+            connection = new MySqlConnection(SiteMaster.ConnectionString);
+            var id_locador = Request.QueryString["id_locador"].ToString();
+            var id_locatario = Request.QueryString["id_locatario"].ToString();
+            var id_imovel = Request.QueryString["id_imovel"].ToString();
+
+            var comando = new MySqlCommand($"SELECT `id`, `id_locador`, `id_locatario`, `id_imovel`, `data_inicio`, `data_fim` FROM `contrato` WHERE 1",connection);
+            comando.ExecuteNonQuery();
+            
         }
 
         protected void btn_imprimir_Click(object sender, EventArgs e)
